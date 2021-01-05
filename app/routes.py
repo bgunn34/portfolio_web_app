28e242459d6d0ee5c06a85c5709c4f70e0095881
Ben Gunn <gunn34@gmail.com>
2021-01-05 13:07:01 -0500

    added base and index html templates. mocked up user and project objects.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username':'Ben'}
+    projects = [
+        {
+            'title':'Pitching Stats Dashboard',
+            'date':'11/19/2020',
+            'github':'github.com/bgunn34/pitchers_dashboard'
+        },
+        {
+            'title':'Batting Stats Dashboard',
+            'date':'12/02/2020',
+            'github':'github.com/bgunn34/batters_dashboard'
+        }
+    ]
+    return render_template('index.html',user=user,projects=projects)
